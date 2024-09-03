72625bfeaf0328a1a1ec743a85cf4e9f32a76eea
Ashreya Jayaram <ashreyajram@gmail.com>
2024-09-03 22:05:02 +0530

    Completed all r exercises on circularly linked lists

--- Chapter-3/r-3.15,16,17.cpp
+++ Chapter-3/r-3.15,16,17.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 using namespace std;
 
-class Exception 
-{
-    private:
-        string msg;
-    public:
-        Exception(const string& err) : msg(err) {};
-        string getError() {return this->msg;}
-};
-
 template <typename T>
 class CLL;
 
@@ -36,6 +27,8 @@ class CLL
         void advance();
         void add(const T& e);
         void remove();
+        int nNodes();
+        void display();
 };
 
 template <typename T>
@@ -56,13 +49,15 @@ bool CLL<T>::isEmpty() const
 template <typename T>
 T& CLL<T>::getFront() const // front element is element following cursor
 {
-    return cursor->next->elem;
+    if (cursor != nullptr) return cursor->next->elem;
+    throw runtime_error("Cannot return front element of empty list.\n");
 }
 
 template <typename T>
-T& CLL<T>::getBack() const
+T& CLL<T>::getBack() const // back element is the cursor
 {
-    return cursor->elem; // back element is element at cursor
+    if (cursor != nullptr) return cursor->elem;
+    throw runtime_error("Cannot return back element of empty list.\n");
 }
 
 template <typename T>
@@ -92,16 +87,55 @@ void CLL<T>::add(const T& e) // add element after cursor
 template <typename T>
 void CLL<T>::remove() // remove element after cursor
 {
-    CNode<T>* old = cursor->next;
-    if (cursor == old)
-    {
+    if (cursor != nullptr) 
+    {	
+        CNode<T>* old = cursor->next;
+        if (old != cursor) 
+        {	
+            cursor->next = old->next;
+            delete old;
+            return;
+        }
         cursor = nullptr;
+        delete old;
+        return;
     }
+    throw runtime_error("Cannot remove element from empty list.\n");
+}
+
+template <typename T>
+int CLL<T>::nNodes()
+{
+    int count = 0;
+    CNode<T>* head = new CNode<T>;
+    head = cursor;
+    if (head == nullptr) return count;
     else
     {
-        cursor->next = old->next;
+        do
+        {
+            head = head->next;
+            count++;
+        } while (head != cursor);
     }
-    delete old;
+    return count;
+}
+
+template <typename T>
+void CLL<T>::display()
+{
+    cout << "Songs in the playlist:" << endl;
+    CNode<T>* head = new CNode<T>;
+    head = cursor;
+    if (head == nullptr) cout << "No songs in the playlist.\n";
+    else
+    {
+        do
+        {
+            cout << head->elem << endl;
+            head = head->next;
+        } while (head != cursor);
+    }
 }
 
 int main()
@@ -113,5 +147,8 @@ int main()
     list.add("Here Comes the Sun");
     list.remove();
     
+    cout << "Number of songs in the playlist is: " << list.nNodes() << endl;
+    list.display();
+
     return 0;
 }
